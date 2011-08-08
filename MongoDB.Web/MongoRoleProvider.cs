85dbbd0a1247f1250849fcdbaced7ac5d5916d77
Cameron McKay <me@cdmckay.org>
2011-08-08 00:31:30 -0400

    Added the RoleProvider skeleton.

---
+++ MongoDB.Web/MongoRoleProvider.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Security;
+
+namespace DigitalLiberationFront.MongoDB.Web.Security {
+    public class MongoRoleProvider : RoleProvider {
+
+        public override string ApplicationName {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public override bool IsUserInRole(string userName, string roleName) {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetRolesForUser(string userName) {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateRole(string roleName) {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole) {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName) {
+            throw new NotImplementedException();
+        }
+
+        public override void AddUsersToRoles(string[] userNames, string[] roleNames) {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] userNames, string[] roleNames) {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetUsersInRole(string roleName) {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles() {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string userNameToMatch) {
+            throw new NotImplementedException();
+        }
+        
+    }
+}
